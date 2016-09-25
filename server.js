6bf0017c58cb36ed24a8ad91edb0108bd083e540
Bakhrom <bahromk@yandex.ru>
2016-09-25 16:30:17 +0000

    pushing changes

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+
+app.get('/helloworld/:id', function(req, res) {
+    res.json({ hello: req.params.id})
+})
+console.log('hello world');
+app.use(express.static(__dirname+'/public', {index: '_'}));
+
+
+var PORT = process.env.PORT || '8080';
+app.listen(PORT, function() {
+    console.log('App listenes on '+PORT+' port')
+})
