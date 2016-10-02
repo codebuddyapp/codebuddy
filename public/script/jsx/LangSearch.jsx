dd9aabec06cec7cda562590d337d46966252d8e3
Dzheky <Zklimenchenko@gmail.com>
2016-10-02 20:04:57 +0000

    new autocomplete

--- public/script/jsx/LangSearch.jsx
+++ public/script/jsx/LangSearch.jsx
@@ -1,46 +1,38 @@
 import React from 'react';
 import $ from 'jquery';
+import AutoComplete from 'material-ui/AutoComplete';
+import MenuItem from 'material-ui/MenuItem';
+
 
 class SearchElement extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            found: []
+            found: ['JavaScript', 'Java', 'Python', 'C', 'C#', 'C++', 'PHP', 'Swift', 'Objective-C'],
+            open: false
+        }
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+    handleChange(text, source) {
+        console.log(text, source);
+        if(source === -1 && this.state.found.indexOf(text) === -1) {
+            $('#search-field').val('');
         }
     }
-    componentDidMount() {
-        var self = this;
-        $('#search-field').on('keydown', function(){
-            $.getJSON('/languages', function(data) {
-                var result = [];
-                data.languages.forEach(function(element) {
-                    var LCElement = element.toLowerCase();
-                    var LCVal = $('#search-field').val().toLowerCase();
-                    if(LCElement.indexOf(LCVal) !== -1 && LCVal !== '') {
-                        result.push(element);
-                    }
-                })
-                self.setState({
-                    found: result
-                })
-            })
-        })
-    }
+
     render() {
-        var foundElement = [];
-        var self = this;
-        if(self.state.found.length) {
-            self.state.found.forEach(function(element) {
-                foundElement.push(<div className='search-element' key={Math.random()}>{element}</div>);
-            }) 
-        } else {
-            foundElement = '';
-        }
         return(
             <div className='search'>
-                {'Search Programming Language'}
-                <input type='text' id='search-field'></input>
-                {foundElement}
+                <AutoComplete
+                    id='search-field'
+                    floatingLabelText="Programming Language"
+                    filter={AutoComplete.caseInsensitiveFilter}
+                    onNewRequest={this.handleChange}
+                    openOnFocus={false}
+                    dataSource={this.state.found}
+                />
+                
             </div>
         )
     }
