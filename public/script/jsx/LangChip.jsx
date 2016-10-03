ad9700364e3405d17c723e5543d4044575719711
Dzheky <Zklimenchenko@gmail.com>
2016-10-03 19:19:13 +0000

    Language search chips added

---
+++ public/script/jsx/LangChip.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import $ from 'jquery';
+import Chip from 'material-ui/Chip';
+
+export default class LangChip extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            chips: this.props.languages
+        }
+        this.styles = {
+          chip: {
+            margin: 4,
+          },
+          wrapper: {
+            display: 'flex',
+            flexWrap: 'wrap',
+          },
+        };
+    }
+    
+    render() {
+        var self = this;
+        var chips = [];
+        this.state.chips.forEach(function(element) {
+         chips.push(<Chip
+                    key={element.id}
+                    onRequestDelete={function() {
+                        self.props.deleteLanguageChip(element.id)
+                    }}
+                    style={self.styles.chip}
+                    >
+                    {element.label}
+                </Chip>);
+        })
+        return (
+            <div id='lang-chips' style={{
+                    display: 'flex',
+                    flexWrap: 'wrap'
+            }}>
+                {chips}
+            </div>
+            
+            
+            )
+    }
+}
